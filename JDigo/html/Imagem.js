b0c7c9d9b53d87f298702f158689b52cad3da3b9
digovc <rodrigo.conspena@gmail.com>
2014-01-29 19:50:22 -0200

    1.3.25 - Reestruturação do projeto javascript para dar início ao JDigo framework. - Nova localidade para as bibliotécas do projeto.

---
+++ JDigo/html/Imagem.js
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+function Imagem() {
+	// HERANÇA
+	
+	Tag.apply(this);
+	Objeto.apply(this);
+
+	// FIM HERANÇA
+
+	// ATRIBUTO
+	// FIM ATRIBUTO
+
+	// MÉTODO
+	
+	this.setObjImage = function(objImage) {
+		// VARIÁVEIS
+		// FIM VARIÁVEIS
+		try {
+			// AÇÕES
+			
+			this.getJq().attr("src", objImage.src)
+			
+			// FIM AÇÕES
+		} catch (e) {
+			new Erro("Erro inesperado.", e);
+		}
+	}
+	
+	// FIM MÉTODO
+
+	/* Construtor */{
+		// VARIÁVEL
+		// AÇÃO
+	}
+
+}
+// INICIALIZAÇÃO
+
+// var objImagem;
+// $(document).ready(function() {
+//	objImagem = new Imagem();
+// });
+
+// FIM INICIALIZAÇÃO
