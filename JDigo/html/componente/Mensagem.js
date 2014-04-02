f462723eb1e145b43de6e0aa76326e4a1b5e426a
Rodrigo <rodrigo.reltar@gmail.com>
2014-04-02 17:02:42 -0300

    1.29.15 - Implementação das mensagens vista pelo usuário.

---
+++ JDigo/html/componente/Mensagem.js
@@ -0,0 +1,153 @@
+// CONSTANTE
+
+var Mensagem_ESTRUTURA_INFO_ALERTA = "";
+var Mensagem_ESTRUTURA_INFO_NEGATIVA = "";
+var Mensagem_ESTRUTURA_INFO_POSITIVA = "";
+
+// FIM CONSTANTE
+// ATRIBUTOS GLOBAIS
+// FIM ATRIBUTOS GLOBAIS
+// CLASSE
+
+/**
+ *
+ */
+function Mensagem(strTitulo, strMsg, intTipo) {
+  // HERANÇA
+
+  ComponenteMain.apply(this);
+  Tag.apply(this);
+  Objeto.apply(this);
+
+  // FIM HERANÇA
+
+  // ATRIBUTO
+
+  var _this = this;
+
+  /**
+   * 0 = alerta; 1 = negativa; 2 = positiva.
+   */
+  var _intTipo = 2;
+
+  this.getIntTipo = function() {
+    return _intTipo;
+  };
+
+  this.setIntTipo = function(intTipo) {
+    _intTipo = intTipo;
+  };
+
+  var _strMsg = "";
+
+  this.getStrMsg = function() {
+    return _strMsg;
+  };
+
+  this.setStrMsg = function(strMsg) {
+    _strMsg = strMsg;
+  };
+
+  var _strTitulo = "";
+
+  this.getStrTitulo = function() {
+    return _strTitulo;
+  };
+
+  this.setStrTitulo = function(strTitulo) {
+    _strTitulo = strTitulo;
+  };
+
+  // FIM ATRIBUTO
+
+  // MÉTODO
+
+  this.montarLayout = function() {
+    // VARIÁVEIS
+
+    var tag;
+
+    // FIM VARIÁVEIS
+    try {
+      // AÇÕES
+
+      switch (this.getIntTipo()) {
+
+      case 0: // Alerta
+        tag = Mensagem_ESTRUTURA_INFO_ALERTA;
+        break;
+
+      case 1: // Negativa
+        tag = Mensagem_ESTRUTURA_INFO_NEGATIVA;
+        break;
+
+      case 2: // Positiva
+        tag = Mensagem_ESTRUTURA_INFO_POSITIVA;
+        break;
+
+      default:
+        tag = Mensagem_ESTRUTURA_INFO_POSITIVA;
+        break;
+      }
+
+      tag = tag.replace("_titulo", this.getStrTitulo());
+      tag = tag.replace("_msg", this.getStrMsg());
+
+      this.setStrEstrutura(tag);
+
+      // FIM AÇÕES
+    } catch (e) {
+      new Erro("Erro inesperado.", e);
+    }
+  };
+
+  this.mostrar = function() {
+    // VARIÁVEIS
+
+    var intTempo;
+    var tag;
+
+    // FIM VARIÁVEIS
+    try {
+      // AÇÕES
+
+      tag = this.toHtml();
+      intTempo = this.getStrMsg().length * 75;
+      $("body").append(tag);
+
+      window.setTimeout(function() {
+
+        $(document).find("#msg").fadeOut("slow");
+
+        window.setTimeout(function() {
+
+          $(document).find("#msg").remove();
+
+        }, intTempo);
+      }, intTempo);
+
+      // FIM AÇÕES
+    } catch (e) {
+      new Erro("Erro inesperado.", e);
+    }
+  };
+
+  // FIM MÉTODO
+
+  /* Construtor */{
+    // VARIÁVEIS
+    // FIM VARIÁVEIS
+    try {
+      // AÇÕES
+
+      this.setIntTipo(intTipo);
+      this.setStrTitulo(strTitulo);
+      this.setStrMsg(strMsg);
+
+      // FIM AÇÕES
+    } catch (e) {
+      new Erro("Erro inesperado.", e);
+    }
+  }
+
+}
---
+++ src/com/digosofter/digowebapp/html/componente/Mensagem.java
@@ -0,0 +1,261 @@
+package com.digosofter.digowebapp.html.componente;
+
+import com.digosofter.digowebapp.AppWeb;
+import com.digosofter.digowebapp.erro.Erro;
+import com.digosofter.digowebapp.html.CssTag;
+import com.digosofter.digowebapp.html.Imagem;
+import com.digosofter.digowebapp.html.Painel;
+
+public class Mensagem extends ComponenteMain {
+
+  public enum EnmTipo {
+    ALERTA, NEGATIVA, POSITIVA
+  }
+
+  private EnmTipo _enmTipo = EnmTipo.POSITIVA;
+
+  private Imagem _imgIcon;
+
+  private Painel _pnlConteudo;
+
+  private Painel _pnlMsg;
+
+  private Painel _pnlTitulo;
+
+  private String _strMensagem = "_msg";
+
+  private String _strTitulo = "_titulo";
+
+  public Mensagem() {
+    // VARIÁVEIS
+    // FIM VARIÁVEIS
+    try {
+      // AÇÕES
+
+      this.setStrId("msg");
+
+      // FIM AÇÕES
+    } catch (Exception ex) {
+
+      new Erro("Erro inesperado.\n", ex);
+
+    } finally {
+    }
+  }
+
+  private EnmTipo getEnmTipo() {
+    return _enmTipo;
+  }
+
+  private Imagem getImgIcon() {
+    // VARIÁVEIS
+
+    String src;
+
+    // FIM VARIÁVEIS
+    try {
+      // AÇÕES
+
+      if (_imgIcon == null) {
+
+        switch (this.getEnmTipo()) {
+
+          case ALERTA:
+            src = "res/media/info_alerta.png";
+            break;
+
+          case NEGATIVA:
+            src = "res/media/info_negativa.png";
+            break;
+
+          case POSITIVA:
+            src = "res/media/info_positiva.png";
+            break;
+
+          default:
+            src = "res/media/info_alerta.png";
+            break;
+        }
+
+        _imgIcon = new Imagem();
+        _imgIcon.setSrc(src);
+      }
+
+      // FIM AÇÕES
+    } catch (Exception ex) {
+
+      new Erro("Erro inesperado.\n", ex);
+
+    } finally {
+    }
+
+    return _imgIcon;
+  }
+
+  private Painel getPnlConteudo() {
+    // VARIÁVEIS
+    // FIM VARIÁVEIS
+    try {
+      // AÇÕES
+
+      if (_pnlConteudo == null) {
+
+        _pnlConteudo = new Painel();
+        _pnlConteudo.addCss(CssTag.getCssMainInst().setBorder(1, "outset",
+            AppWeb.getI().getObjPaletaCor().getStrCorBorda()));
+        _pnlConteudo.addCss(CssTag.getCssMainInst().setBorderRadius(5, 5, 5, 5));
+        _pnlConteudo.addCss(CssTag.getCssMainInst().setBoxShadow(0, 2, 2, 2,
+            AppWeb.getI().getObjPaletaCor().getStrCorBorda()));
+        _pnlConteudo.addCss(CssTag.getCssMainInst().setDisplay("table"));
+        _pnlConteudo.addCss(CssTag.getCssMainInst().setPadding(5, "px"));
+        _pnlConteudo.addCss(CssTag.getCssMainInst().setBackgroundColor("white"));
+        _pnlConteudo.addCss(CssTag.getCssMainInst().addCss("margin", "auto"));
+      }
+
+      // FIM AÇÕES
+    } catch (Exception ex) {
+
+      new Erro("Erro inesperado.\n", ex);
+
+    } finally {
+    }
+
+    return _pnlConteudo;
+  }
+
+  private Painel getPnlMsg() {
+    // VARIÁVEIS
+    // FIM VARIÁVEIS
+    try {
+      // AÇÕES
+
+      if (_pnlMsg == null) {
+
+        _pnlMsg = new Painel();
+        _pnlMsg.setStrConteudo(this.getStrMensagem());
+      }
+
+      // FIM AÇÕES
+    } catch (Exception ex) {
+
+      new Erro("Erro inesperado.\n", ex);
+
+    } finally {
+    }
+
+    return _pnlMsg;
+  }
+
+  protected Painel getPnlTitulo() {
+    // VARIÁVEIS
+    // FIM VARIÁVEIS
+    try {
+      // AÇÕES
+
+      if (_pnlTitulo == null) {
+
+        _pnlTitulo = super.getPnlTitulo();
+        _pnlTitulo.setStrConteudo(this.getStrTitulo());
+        _pnlTitulo.addCss(CssTag.getCssMainInst().setFontNegrito());
+        _pnlTitulo.addCss(CssTag.getCssMainInst().setMarginBottom(5));
+        _pnlTitulo.addCss(CssTag.getCssMainInst().setPaddingTop(5));
+        _pnlTitulo.addCss(CssTag.getCssMainInst().setBorderTop(1,
+            AppWeb.getI().getObjPaletaCor().getStrCorBorda()));
+      }
+
+      // FIM AÇÕES
+    } catch (Exception ex) {
+
+      new Erro("Erro inesperado.\n", ex);
+
+    } finally {
+    }
+
+    return _pnlTitulo;
+  }
+
+  private String getStrMensagem() {
+    return _strMensagem;
+  }
+
+  private String getStrTitulo() {
+    return _strTitulo;
+  }
+
+  @Override
+  protected void montarLayout() {
+
+    super.montarLayout();
+
+    // VARIÁVEIS
+    // FIM VARIÁVEIS
+    try {
+      // AÇÕES
+
+      this.getPnlConteudo().setTagPai(this);
+      this.getImgIcon().setTagPai(this.getPnlConteudo());
+      this.getPnlTitulo().setTagPai(this.getPnlConteudo());
+      this.getPnlMsg().setTagPai(this.getPnlConteudo());
+
+      // FIM AÇÕES
+    } catch (Exception ex) {
+
+      new Erro("Erro inesperado.\n", ex);
+
+    } finally {
+    }
+  }
+
+  @Override
+  protected void setCss(CssTag tagCss) {
+
+    super.setCss(tagCss);
+
+    // VARIÁVEIS
+    // FIM VARIÁVEIS
+    try {
+      // AÇÕES
+
+      this.addCss(tagCss.setPosition("fixed"));
+      this.addCss(tagCss.setWidth(100, "%"));
+      this.addCss(tagCss.setTop(15));
+      this.addCss(tagCss.setZ(100));
+
+      // FIM AÇÕES
+    } catch (Exception ex) {
+
+      new Erro("Erro inesperado.\n", ex);
+
+    } finally {
+    }
+  }
+
+  public void setEnmTipo(EnmTipo enmTipo) {
+    _enmTipo = enmTipo;
+  }
+
+  public void setStrMensagem(String strMensagem) {
+    _strMensagem = strMensagem;
+  }
+
+  public void setStrTitulo(String strTitulo) {
+
+    super.setStrTitulo(strTitulo);
+
+    // VARIÁVEIS
+    // FIM VARIÁVEIS
+    try {
+      // AÇÕES
+
+      _strTitulo = strTitulo;
+
+      // FIM AÇÕES
+    } catch (Exception ex) {
+
+      new Erro("Erro inesperado.\n", ex);
+
+    } finally {
+    }
+  }
+
+}
