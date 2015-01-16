532bea4537e2fc48721c2928771029c3a9d2d87e
Rodrigo Vieira <rodrigo.conspena@gmail.com>
2015-01-16 10:20:12 -0200

    3.7.6
    - Vários ajustes.

--- JDigo/websocket/WebSocketMain.js
+++ JDigo/websocket/WebSocketMain.js
@@ -11,88 +11,110 @@ var WebSocketMain_FNC_MSG_POSITIVA = 701000;
 // CLASSE
 function WebSocketMain(strUrl) {
   // HERANÇA
+
+  Objeto.apply(this);
+
   // FIM HERANÇA
 
   // ATRIBUTO
 
   var _this = this;
 
-  var _fncOnMessage = null;
+  // ATRIBUTO ABSTRATO
+
+  var _strHost;
+  var _strWsPagina;
+
+  // FIM ATRIBUTO ABSTRATO
+
+  var _fncOnMessage;
 
   /**
    * Método que deve ser implementado pela classe de instância para receber e
    * processar as mensagens vindas do servidor.
    */
   this.getFncOnMessage = function() {
-    
-    
+
     try {
-      
 
-      if (_fncOnMessage == null) {
+      if (_fncOnMessage != null) {
 
-        _fncOnMessage = function(evt) {
-          
-
-          var objWsInterlocutor;
-
-          
-          try {
-            
-
-            objWsInterlocutor = JSON.parse(evt.data);
-            _this.processarObjWsInterlocutor(objWsInterlocutor);
-
-            
-          } catch (e) {
-            new Erro("Erro inesperado.", e);
-          }
-        };
-
+        return _fncOnMessage;
       }
 
-      
+      _fncOnMessage = function(evt) {
+
+        var objWsInterlocutor;
+
+        try {
+
+          objWsInterlocutor = JSON.parse(evt.data);
+          _this.processarObjWsInterlocutor(objWsInterlocutor);
+
+        } catch (e) {
+
+          new Erro("Erro inesperado.", e);
+        }
+      };
     } catch (e) {
+
       new Erro("Erro inesperado.", e);
     }
 
     return _fncOnMessage;
   };
 
-  var _objWebSocket = null;
+  var _objWebSocket;
 
   this.getObjWebSocket = function() {
-    
-    
+
     try {
-      
 
       if (!"WebSocket" in window) {
+
         new Erro(STR_MENSAGEM_00001, null);
+        return false;
       }
 
-      if (_objWebSocket == null) {
+      if (_objWebSocket != null) {
 
-        _objWebSocket = new WebSocket(this.getStrUrl());
-        _objWebSocket.onmessage = this.getFncOnMessage();
+        return _objWebSocket;
       }
 
-      
+      _objWebSocket = new WebSocket(this.getStrUrl());
+
+      _objWebSocket.onmessage = this.getFncOnMessage();
+
     } catch (e) {
+
       new Erro("Erro inesperado.", e);
     }
 
     return _objWebSocket;
   };
 
-  var _strUrl = "";
+  var _strUrl;
 
   this.getStrUrl = function() {
-    return _strUrl;
-  };
 
-  this.setStrUrl = function(strUrl) {
-    _strUrl = strUrl;
+    try {
+
+      if (!Utils.getBooStrVazia(_strUrl)) {
+
+        return _strUrl;
+      }
+
+      _strUrl = "ws://_host/ws/_ws_pag";
+
+      _strUrl = _strUrl.replace("_host", _this.getStrHost());
+      _strUrl = _strUrl.replace("_ws_pag", _this.getStrWsPagina());
+
+    } catch (e) {
+
+      new Erro("Erro inesperado.", e);
+    }
+
+    return _strUrl;
   };
 
   // FIM ATRIBUTO
@@ -100,99 +122,85 @@ function WebSocketMain(strUrl) {
   // MÉTODO
 
   this.abrirConexao = function() {
-    
+
     try {
-      
 
       this.getObjWebSocket();
 
     } catch (e) {
+
       new Erro("Erro inesperado.", e);
     }
   };
 
   this.enviar = function(objWsInterlocutor) {
-    
+
     try {
-      
 
       this.getObjWebSocket().send(JSON.stringify(objWsInterlocutor));
 
     } catch (e) {
+
       new Erro("Erro inesperado.", e);
     }
   };
 
   this.processarMsgErro = function(strMsgErro) {
-    
 
     var msg;
 
-    
     try {
-      
 
       if (Utils.getBooStrVazia(strMsgErro)) {
+
         return false;
       }
 
       msg = new Mensagem("Erro", strMsgErro, Mensagem_TIPO_NEGATIVA);
+
       msg.mostrar();
 
-      
     } catch (e) {
+
       new Erro("Erro inesperado.", e);
     }
   };
 
   this.processarMsgPositiva = function(strMsgPositiva) {
-    
 
     var msg;
 
-    
     try {
-      
 
       if (Utils.getBooStrVazia(strMsgPositiva)) {
+
         return false;
       }
 
       msg = new Mensagem("Ok " + objUsuario.getStrNome(), strMsgPositiva, Mensagem_TIPO_POSITIVA);
+
       msg.mostrar();
 
-      
     } catch (e) {
+
       new Erro("Erro inesperado.", e);
     }
   };
 
   this.processarObjWsInterlocutor = function(objWsInterlocutor) {
-    
-    
-    try {
-      
-      
-    } catch (e) {
-      new Erro("Erro inesperado.", e);
-    }
   };
 
   // FIM MÉTODO
 
   /* Construtor */
   {
-    
-    
     try {
-      
 
-      this.setStrUrl(strUrl);
+      this.abrirConexao();
 
-      
     } catch (e) {
+
       new Erro("Erro inesperado.", e);
     }
   }
-
 }
